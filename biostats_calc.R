9d4ec7d37b1ac204a5fc487e20a564dbd9a83452
“Rachel <rachelshadoff@hotmail.ca>
2025-04-30 13:26:35 -0400

    Added evaluation metrics (confusion matrix and ROC AUC for hair and eye prediction models)

--- biostats_calc.R
+++ biostats_calc.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(dplyr)
 library(caret)
+library(pROC)
+library(reshape2)
 
 df <- read.csv("PhenotypePredictions.tsv", sep="\t")
 
@@ -26,6 +28,11 @@ snp_df$X <- sapply(snp_df$X, as.numeric)
 combined_df <- left_join(df_clean, snp_df, by = c("Sample" = "X"))
 combined_df_clean <- na.omit(combined_df)
 
+SNP_cols = colnames(combined_df_clean[6:29])
+for (col in SNP_cols) {
+  combined_df_clean[[col]] <- factor(combined_df_clean[[col]], levels = unique(combined_df_clean[[col]]))
+}
+
 # Hair Colour Model
 hair_levels <- union(unique(combined_df_clean$Hair.Predicted), unique(combined_df_clean$Hair.Self.ID))
 combined_df_clean$Hair.Predicted <- factor(combined_df_clean$Hair.Predicted, levels = hair_levels)
@@ -40,7 +47,9 @@ test_data_hair <- combined_df_clean[-train_index_hair, ]
 fit_control_hair <- trainControl(method = "cv", number = 5, sampling = "up") # Upsampled due to low counts in minority class (blonde) 
 
 # Remove SNPs with only one level
-SNP_cols = colnames(combined_df_clean[6:29])
+
+
+
 valid_SNP_cols_hair <- SNP_cols[sapply(train_data_hair[ , SNP_cols], function(x) length(unique(x)) > 1)]
 
 SNP_str_hair = paste(valid_SNP_cols_hair, collapse = " + ")
@@ -50,7 +59,7 @@ model_hair <- train(as.formula(model_formula_hair),
                     method = "rf",
                     trControl = fit_control_hair)
 pred_hair <- predict(model_hair, newdata = test_data_hair)
-
+prob_hair <- predict(model_hair, newdata = test_data_hair, type = "prob")
 # Eye Colour Model
 eye_levels <- union(unique(combined_df_clean$Eye.Predicted), unique(combined_df_clean$Eye.Self.ID))
 combined_df_clean$Eye.Predicted <- factor(combined_df_clean$Eye.Predicted, levels = eye_levels)
@@ -77,9 +86,67 @@ model_eye <- train(as.formula(model_formula_eye),
                     method = "rf",
                     trControl = fit_control_eye)
 pred_eye <- predict(model_eye, newdata = test_data_eye)
+prob_eye <- predict(model_eye, newdata = test_data_eye, type = "prob")
 
 
 
+# Evaluation
+conf_matrix_eye <- confusionMatrix(pred_eye, test_data_eye$Eye.Self.ID)
+print(conf_matrix_eye)
+# Actual labels
+true_labels <- test_data_eye$Eye.Self.ID
+
+# Convert to one-hot encoding
+actual_mat <- model.matrix(~ true_labels - 1)
+colnames(actual_mat) <- levels(true_labels)
+
+# Create an empty list to store ROC curves
+roc_list <- list()
+auc_list <- c()
+
+# Loop through each class
+for (class in levels(true_labels)) {
+  roc_obj <- roc(actual_mat[, class], prob_eye[, class])
+  auc_val <- auc(roc_obj)
+  auc_list[class] <- auc_val
+  
+  plot(roc_obj, main = paste("ROC Curve -", class), col = "blue", lwd = 2)
+  legend("bottomright", legend = paste("AUC =", round(auc_val, 3)), col = "blue", lwd = 2)
+  
+  roc_list[[class]] <- roc_obj
+}
+print(auc_list)
+
+
+# Hair
+
+
+conf_matrix_hair <- confusionMatrix(pred_hair, test_data_hair$Hair.Self.ID)
+print(conf_matrix_hair)
+
+# Actual labels
+true_labels <- test_data_hair$Hair.Self.ID
+
+# Convert to one-hot encoding
+actual_mat <- model.matrix(~ true_labels - 1)
+colnames(actual_mat) <- levels(true_labels)
+
+# Create an empty list to store ROC curves
+roc_list <- list()
+auc_list <- c()
+
+# Loop through each class
+for (class in levels(true_labels)) {
+  roc_obj <- roc(actual_mat[, class], prob_hair[, class])
+  auc_val <- auc(roc_obj)
+  auc_list[class] <- auc_val
+  
+  plot(roc_obj, main = paste("ROC Curve -", class), col = "blue", lwd = 2)
+  legend("bottomright", legend = paste("AUC =", round(auc_val, 3)), col = "blue", lwd = 2)
+  
+  roc_list[[class]] <- roc_obj
+}
+print(auc_list)
 
 
 
